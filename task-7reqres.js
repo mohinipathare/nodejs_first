2ab688f6a6e52c35b1150587756f869483910121
mohinipathare <87515869+mohinipathare@users.noreply.github.com>
2023-03-24 22:19:13 +0530

    Based on the url the user hits return custom response
    home page,about us page, welcome to node js project

--- task-7reqres.js
+++ task-7reqres.js
@@ -1,10 +1,31 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
+    // res.setHeader('content-type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>hi</title></head>');
+    // res.write('<h1>Welcome to my Node Js project</h1>');
+    if(req.url=="/home")
+    {
     res.setHeader('content-type','text/html');
     res.write('<html>');
-    res.write('<head><title>hi</title></head>');
-    res.write('<h1>Welcome to my Node Js project</h1>');
-    console.log(res);
+    res.write('<head><title>request response</title></head>');
+    res.write('<h1>Welcome to home</h1>');
+
+    }
+    else if(req.url=='/about')
+    {
+    res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>request response</title></head>');
+    res.write('<h1>Welcome to about us page</h1>');
+    }
+    else if(req.url=='/node'){
+    res.setHeader('content-type','text/html');
+    res.write('<html>');
+    res.write('<head><title>request response</title></head>');
+    res.write('<h1> Welcome to my Node Js project</h1>');
+    }
+    //console.log(res);
    res.end();
 })
 server.listen(3000);
